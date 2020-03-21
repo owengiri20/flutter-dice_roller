d3ec15e0870f6256ea1eb674b46a86a04437cb9e
owengiri20 <owengiri20@gmail.com>
2020-03-21 14:00:31 +0800

    random nums

--- lib/main.dart
+++ lib/main.dart
@@ -21,42 +21,45 @@ void main() {
   );
 }
 
-
-
 class DicePage extends StatefulWidget {
-
   @override
   _DicePageState createState() => _DicePageState();
 }
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
 
   @override
   Widget build(BuildContext context) {
-      return Center(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: FlatButton(
-                onPressed: () {setState(() {
-                  leftDiceNumber = 3;
-                });},
-                child: Image.asset("./images/dice$leftDiceNumber.png"),
-              ),
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset("./images/dice$leftDiceNumber.png"),
             ),
-            SizedBox(
-              width: 3,
+          ),
+          SizedBox(
+            width: 3,
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber= Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset("./images/dice$rightDiceNumber.png"),
             ),
-            Expanded(
-              child: FlatButton(
-                onPressed: () {print("owen");},
-                child: Image.asset("./images/dice1.png"),
-              ),
-            )
-          ],
-        ),
-      );
+          )
+        ],
+      ),
+    );
   }
 }
-
